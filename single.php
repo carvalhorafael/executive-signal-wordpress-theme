638e8f20b70a784deedd8c2a75467b0c90c7eb1e
Rafael Carvalho <falecomigo@rafaelcarvalho.tv>
2026-05-31 16:45:49 -0300

    Add adjacent article navigation

--- single.php
+++ single.php
@@ -15,6 +15,7 @@ get_header();
 
 		get_template_part( 'template-parts/content', 'single' );
 		get_template_part( 'template-parts/share-post' );
+		get_template_part( 'template-parts/post-navigation' );
 		get_template_part( 'template-parts/related-posts' );
 
 		if ( comments_open() || get_comments_number() ) {
