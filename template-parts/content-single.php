2e5d0dcd42588dfc2f18dfc92a85bdf560b6a958
Rafael Carvalho <falecomigo@rafaelcarvalho.tv>
2026-06-04 16:50:54 -0300

    Enhance single post editorial rails

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -6,6 +6,10 @@
  */
 
 ?>
+<?php
+$article_content_data = executive_signal_prepare_article_content( str_replace( ']]>', ']]&gt;', apply_filters( 'the_content', get_the_content() ) ) );
+$has_right_rail       = ! empty( $article_content_data['table_of_contents'] ) || is_active_sidebar( 'post-right' );
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry entry--single' ); ?> itemscope itemtype="https://schema.org/BlogPosting">
 	<header class="es-article-hero" data-layout="text-only">
 		<div class="es-article-hero__content">
@@ -23,11 +27,26 @@
 
 	</header>
 
-	<div class="entry__content es-article-prose" itemprop="articleBody">
-		<?php
-		the_content();
-		wp_link_pages();
-		?>
+	<div class="entry__body-layout">
+		<?php if ( is_active_sidebar( 'post-left' ) ) : ?>
+			<aside class="entry__widget-area entry__widget-area--left" aria-label="<?php esc_attr_e( 'Post left rail', 'executive-signal-wordpress-theme' ); ?>">
+				<?php dynamic_sidebar( 'post-left' ); ?>
+			</aside>
+		<?php endif; ?>
+
+		<div class="entry__content es-article-prose" itemprop="articleBody">
+			<?php
+			echo $article_content_data['content']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Content is filtered through the WordPress content pipeline above.
+			wp_link_pages();
+			?>
+		</div>
+
+		<?php if ( $has_right_rail ) : ?>
+			<aside class="entry__widget-area entry__widget-area--right" aria-label="<?php esc_attr_e( 'Post right rail', 'executive-signal-wordpress-theme' ); ?>">
+				<?php executive_signal_render_article_table_of_contents( $article_content_data['table_of_contents'] ); ?>
+				<?php dynamic_sidebar( 'post-right' ); ?>
+			</aside>
+		<?php endif; ?>
 	</div>
 
 	<footer class="entry__footer">
